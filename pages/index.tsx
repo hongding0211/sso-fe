2e11d90d26b92ca1d16383deb869f0b15df3480a
hongding0211 <keith.dh@hotmail.com>
2022-10-31 21:47:38 +0800

    feat: style

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,8 +75,8 @@ export default function Home() {
   return (
     <>
       <div className='h-screen w-screen bg-bg_light dark:bg-bg_dark bg-cover flex justify-center items-center min-w-max'>
-        <div className='shadow-xl p-8 md:p-12 bg-white dark:bg-black m-6 w-full max-w-[450px] md:max-w-none rounded-xl flex flex-col justify-between md:flex-row md:items-center md:w-[893px] md:h-[552px] md:rounded-2xl'>
-          <div className='h-[120px] md:h-full bg-contain bg-no-repeat relative bg-illustrator_s bg-[right_-40px_top] md:w-[360px] md:bg-illustrator_l md:bg-bottom'>
+        <div className='relative top-[-5vh] md:static md:top-none shadow-xl p-8 md:p-12 bg-white dark:bg-black m-6 w-full max-w-[450px] md:max-w-none rounded-xl flex flex-col justify-between md:flex-row md:items-center md:w-[893px] md:h-[552px] md:rounded-2xl'>
+          <div className='h-[100px] md:h-full bg-contain bg-no-repeat relative bg-illustrator_s bg-[right_-40px_top] md:w-[360px] md:bg-illustrator_l md:bg-bottom'>
             <div>
               <p className='text-blue-500 font-medium text-3xl md:text-4xl'>Single Sign On</p>
               <p className='text-zinc-900 mt-2 font-medium dark:text-zinc-200'>统一登录验证平台</p>
@@ -86,7 +86,7 @@ export default function Home() {
             <p className='font-medium md:text-4xl text-zinc-900 hidden md:block dark:text-zinc-200'>登录</p>
             <div onKeyDown={handleKeyDown} className='mb-8 mb:my-none'>
               <Input label='邮箱 / 电话' value={emailInput} onChange={handleChangeEmailInput} ref={emailInputRef}/>
-              <div className='mt-8'>
+              <div className='mt-2'>
                 <Input label='密码' value={passwordInput} type='password' onChange={handleChangePassword}/>
                 <p className='text-sm text-zinc-600 underline cursor-pointer mt-3 dark:text-zinc-400' onClick={handleClickForget}>忘记密码</p>
               </div>
@@ -97,7 +97,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <footer className='absolute bottom-6'>
+        <footer className='absolute bottom-4 md:bottom-6'>
           <span className='text-sm text-zinc-600 dark:text-zinc-400'>Copyright © {`${new Date(Date.now()).getFullYear()}`} SSO <a className='underline' target='_blank' href='https://hong97.ltd' rel="noreferrer">hong97.ltd</a></span>
         </footer>
       </div>
